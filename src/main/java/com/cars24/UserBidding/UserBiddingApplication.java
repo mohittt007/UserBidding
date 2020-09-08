233c9dd9c7a0aebc5e0a25a6a3b3007f0c24422d
mohittt007 <mohittt007@gmail.com>
2020-09-08 22:40:18 +0530

    Project setup

---
+++ src/main/java/com/cars24/UserBidding/UserBiddingApplication.java
@@ -0,0 +1,13 @@
+package com.cars24.UserBidding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBiddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBiddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cars24/UserBidding/UserBiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars24.UserBidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
